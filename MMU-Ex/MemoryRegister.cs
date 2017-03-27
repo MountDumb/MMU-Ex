22ba1d3cbd53458b22edaaa40e7880dc04e63565
MountDumb <torb3755@edu.eal.dk>
2017-03-27 11:59:44 +0200

    Add project files.

---
+++ MMU-Ex/MemoryRegister.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMU_Ex
+{
+    public class MemoryRegister
+    {
+        bool[] _adresses;
+        public MemoryRegister()
+        {
+            _adresses = new bool[100];
+        }
+    }
+}
---
+++ MMU-Ex/TProcess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMU_Ex
+{
+    public class TProcess
+    {
+        private int _allocatedMemorySize;
+
+        public int AllocatedMemorySize
+        {
+            get { return _allocatedMemorySize; }
+            set
+            {
+                if (value > 0 && value < 7)
+                {
+                    _allocatedMemorySize = value;
+                }
+                else
+                {
+                    throw new Exception("Memory allocation is outside of the accepted range. Please alocate betweed 1 and 6 pages");
+                }
+            }
+        }
+
+
+    }
+}
